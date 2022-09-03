014d91f425b98cea3c6909049a5ddc3699fd48ec
Deha Efecan Süer <dehasuer97@gmail.com>
2022-09-03 14:04:04 +0300

    file added

---
+++ CodewarsKatasSwift.playground/Pages/TakeTheFirstNElement.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+func take(_ arr: [Int], _ n: Int) -> [Int] {
+  let arr2 = arr.prefix(n)
+  let arr3 = Array(arr2)
+  return arr3
+}
+
+
+//Create a function take that takes an Array<Int> and an Int, n, and returns an Array<Int> containing the first up to n elements from the array.
+
