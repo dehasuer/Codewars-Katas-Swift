17c398cb2522d4d922adcdd558175030abac25d4
Deha Efecan Süer <dehasuer97@gmail.com>
2022-09-04 10:06:54 +0300

    file added

---
+++ CodewarsKatasSwift.playground/Pages/ReversedSequence.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+func reverseSeq(n: Int) -> [Int] {
+  var reversedArray = [Int]()
+  for i in 1...n {
+    reversedArray.append(i)
+  }
+  return reversedArray.reversed()
+}
+
+
+//Build a function that returns an array of integers from n to 1 where n>0.
+
