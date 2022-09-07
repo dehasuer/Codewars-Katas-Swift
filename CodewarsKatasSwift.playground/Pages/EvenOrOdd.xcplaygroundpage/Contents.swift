10e6eb082dc7ccfce68024c77133db713f7c73e8
Deha Efecan Süer <dehasuer97@gmail.com>
2022-09-07 11:08:13 +0300

    file added

---
+++ CodewarsKatasSwift.playground/Pages/EvenOrOdd.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+func evenOrOdd(_ number:Int) -> String {
+  number % 2 == 0 ? "Even" : "Odd"
+}
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
