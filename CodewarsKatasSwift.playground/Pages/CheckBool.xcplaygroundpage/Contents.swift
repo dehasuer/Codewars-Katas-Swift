01440d9db0974be5383b46cf64449a41542b10b0
Deha Efecan Süer <dehasuer97@gmail.com>
2022-09-07 11:03:47 +0300

    file added

---
+++ CodewarsKatasSwift.playground/Pages/CheckBool.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+func boolToWord(_ bool: Bool) -> String {
+  bool ? "Yes" : "No"
+}
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
