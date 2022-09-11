f47b559ec6ad42b26be44fcc8f613d68daddae03
Deha Efecan Süer <dehasuer97@gmail.com>
2022-09-11 20:05:52 +0300

    file added

---
+++ CodewarsKatasSwift.playground/Pages/SimpleMultplication.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+func simpleMultiplication(_ num: Int) -> Int {
+  num % 2 == 0 ? num * 8 : num * 9
+}
+ //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
